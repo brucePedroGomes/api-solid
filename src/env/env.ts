308ea4d613d33ea0cef4878ef9b6c7fd17a378d1
Pedro Gomes <pedroleinar@gmail.com>
2023-03-17 21:49:50 -0300

    feat: :sparkles: docker-compose and model of user/check-in/gym

--- src/env/env.ts
+++ src/env/env.ts
@@ -4,6 +4,7 @@ import { z } from 'zod'
 const envSchema = z.object({
    NODE_ENV: z.enum(['dev', 'test', 'production']),
    PORT: z.coerce.number().default(3333),
+   DATABASE_URL: z.string()
 })
 
 export const env = envSchema.parse(process.env)
